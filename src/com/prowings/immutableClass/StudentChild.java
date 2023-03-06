3991f1b32b5162c2c346326dcb7b701a45b987dd
JD <jayprowings@gmail.com>
2023-03-06 09:03:38 +0530

    immutable class demo

---
+++ src/com/prowings/immutableClass/StudentChild.java
@@ -0,0 +1,21 @@
+/*
+ * package com.prowings.immutableClass;
+ * 
+ * public class StudentChild extends Student{
+ * 
+ * 
+ * 
+ * 
+ * public StudentChild() { super(); // TODO Auto-generated constructor stub }
+ * 
+ * public StudentChild(int roll, String name, String address) { super(roll,
+ * name, address); // TODO Auto-generated constructor stub }
+ * 
+ * public void changeStudent() {
+ * 
+ * this.roll = 234; this.name = "aaaaa"; this.address = "zzzz";
+ * 
+ * }
+ * 
+ * }
+ */

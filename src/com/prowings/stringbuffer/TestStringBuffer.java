1e30f99cbf4cc22050e1bf96b1613bd65eafeaa4
JD <jayprowings@gmail.com>
2023-03-06 07:48:32 +0530

    latest codes

--- src/com/prowings/stringbuffer/TestStringBuffer.java
+++ src/com/prowings/stringbuffer/TestStringBuffer.java
@@ -5,24 +5,19 @@ public class TestStringBuffer {
 	public static void main(String[] args) {
 		
 		
-		StringBuffer sb1 = new StringBuffer("hello");
+		StringBuffer sb1 = new StringBuffer();
+		System.out.println("initial capacity : "+sb1.capacity());
+
+		sb1.append("helloooooooooooooy");
 		
-		System.out.println(sb1.capacity());
+		System.out.println("capacity after adding contents : "+sb1.capacity());
+		
 		System.out.println(sb1.length());
 
-		sb1.trimToSize();
+//		sb1.trimToSize();
 		
-		System.out.println(sb1.capacity());
+//		System.out.println(sb1.capacity());
 
-		sb1.append("world");
-		
-		System.out.println(sb1);
-		
-		String s1 = "hello";
-		
-		s1.concat("world");
-		System.out.println(s1);
-		
 	}
 
 }
